c7de7c7fbeba3f5c167726f50565396c9fe77a49
Ove Kargard <ove@rightscale.com>
2012-02-21 16:29:05 -0800

    pushing my first cookbook and helloworld recipe

---
+++ cookbooks/my-first-cookbook/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "reallyreallsuperbad.com"
+maintainer_email "satan@hell.org"
+license          "All rights reserved Cause you have none, well do what you want and leave me alone!"
+description      "Installs/Configures my-first-cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+recipe 		 "my-first-cookbook::helloworld", "this just prints helloworld out to the dashboard"
